86903e0cd7584706395926f814456f6caf0e1539
reborn <zakaden@gmail.com>
2020-02-17 13:13:07 -0800

    Generalize generatePdf scripts for multiple usecases
    Write a general generatePdf function wrapping puppeteer, with generatePdfCli
    as one consumer of generatePdf. Add option to return raw pdf contents to
    standard out.

--- generatePdf.js
+++ generatePdf.js
@@ -1,34 +1,63 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
+DEFAULT_PRINT_OPTIONS = {
+  format: "Letter",
+  margin: {
+    top: "0.5in",
+    bottom: "0.5in",
+    right: "0.5in",
+    left: "0.5in"
+  }
+}
+
+async function getUrlPdf(browser, url, outputPath, optionOverrides) {
+  let pdfOptions = optionOverrides || DEFAULT_PRINT_OPTIONS;
+
+  pdfOptions.path = outputPath;
+
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle2" })
+  return await page.pdf(pdfOptions)
+}
+
+async function getUrlPdfCli() {
+  function parseOptionOverridesParam(optionOverridesParam) {
+    try {
+      return optionOverridesParam && JSON.parse(optionOverridesParam);
+    } catch(exception) {
+      throw Error("Error parsing option ovverides as JSON: " + exception.message);
+    }
+  }
   const argv = require("yargs")
     .option("url", {
       alias: "u",
       describe: "URL to generate PDF from"
     })
-    .option("outputPath", {
+    .option("writePath", {
+      alias: "w",
+      describe: "path where generated PDF file should be created, if parameter is supplied"
+    })
+    .option("optionOverrides", {
       alias: "o",
-      describe: "path where generated PDF file should be created"
+      describe: "json representing overrides to default pdf print parameters"
     })
     .demandOption(
-      ["url", "outputPath"],
-      "Please provide required url and outputPath arguments"
+      ["url" ],
+      "Please provide required url argument "
     )
     .help().argv;
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(argv.url, { waitUntil: "networkidle2" });
-  await page.pdf({
-    path: argv.outputPath,
-    format: "Letter",
-    margin: {
-      top: "0.5in",
-      bottom: "0.5in",
-      right: "0.5in",
-      left: "0.5in"
-    }
-  });
+  try {
+    const optionOverrides = parseOptionOverridesParam(argv.optionOverrides);
+    const browser = await puppeteer.launch();
+    var pdfContents = await getUrlPdf(browser, argv.url, argv.outputPath, argv.optionOverrides)
+    process.stdout.write(pdfContents);
+    await browser.close();
+    process.exit(0);
+  } catch(exception) {
+    process.stderr.write(exception.message + '\n');
+    process.exit(1);
+  }
+}
 
-  await browser.close();
-})();
+module.exports = {getUrlPdf, getUrlPdfCli}
